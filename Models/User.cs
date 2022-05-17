28ef712b94d01b60f1257e339003a4c911461d8d
Quentin Sims <quentinsims89@gmail.com>
2022-05-17 18:44:04 +0200

    Added necessary views for teams

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace SecurityMobileAppWeb.Models
+{
+    public class User : BaseEntity
+    {
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("Team")]
+        public string Team { get; set; }
+    }
+}
